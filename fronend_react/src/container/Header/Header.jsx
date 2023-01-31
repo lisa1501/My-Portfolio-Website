4d5b244b26a1599a74193207e87f8575d4da6c0d
halisa <halisasaipulaa@gmail.com>
2023-01-30 23:00:02 -0800

    Higher-Order Components--rendered Social media on the page

--- fronend_react/src/container/Header/Header.jsx
+++ fronend_react/src/container/Header/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-import { images } from '../../constants'
+import { AppWrap } from '../../wrapper';
+
+import { images } from '../../constants';
 
 import './Header.scss';
 
@@ -18,7 +20,7 @@ const scaleVariants = {
 
 const Header = () => {
     return (
-        <div id="home" className='app__header app__flex'>
+        <div className='app__header app__flex'>
             <motion.div
                 whileInView={{ x:[-100,0], opacity:[0,1]}}
                 transition={{ duration:0.5}}
@@ -68,4 +70,4 @@ const Header = () => {
     )
 }
 
-export default Header
+export default AppWrap(Header, 'home');
